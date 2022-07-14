eb5567e2d32784d6bf47fa30954d8fdae24ddf1d
Ivan Mladenov <ivan.mladenov@concordeu.com>
2022-07-14 17:38:48 +0300

    Implement append with eventstoredb

--- 04-AppendingEvents.EventStoreDB/AppendingEvents.cs
+++ 04-AppendingEvents.EventStoreDB/AppendingEvents.cs
@@ -1,60 +1,25 @@
-using System.Text.Json;
+using Domain.Common;
+using Domain.Events;
+using Domain.ValueObjects;
 using EventStore.Client;
 using FluentAssertions;
+using Newtonsoft.Json;
+using System.Text;
 using Xunit;
 
 namespace IntroductionToEventSourcing.AppendingEvents;
 
-// EVENTS
-public record ShoppingCartOpened(
-    Guid ShoppingCartId,
-    Guid ClientId
-);
-
-public record ProductItemAddedToShoppingCart(
-    Guid ShoppingCartId,
-    PricedProductItem ProductItem
-);
-
-public record ProductItemRemovedFromShoppingCart(
-    Guid ShoppingCartId,
-    PricedProductItem ProductItem
-);
-
-public record ShoppingCartConfirmed(
-    Guid ShoppingCartId,
-    DateTime ConfirmedAt
-);
-
-public record ShoppingCartCanceled(
-    Guid ShoppingCartId,
-    DateTime CanceledAt
-);
-
-// VALUE OBJECTS
-public record PricedProductItem(
-    ProductItem ProductItem,
-    decimal UnitPrice
-)
-{
-    public Guid ProductId => ProductItem.ProductId;
-    public int Quantity => ProductItem.Quantity;
-
-    public decimal TotalPrice => Quantity * UnitPrice;
-}
-
-public record ProductItem(
-    Guid ProductId,
-    int Quantity
-);
-
 public class GettingStateFromEventsTests
 {
-    private Task<IWriteResult> AppendEvents(EventStoreClient eventStore, string streamName, object[] events,
+    private async Task<IWriteResult> AppendEvents(EventStoreClient eventStore, string streamName, object[] events,
         CancellationToken ct)
     {
         // TODO: Fill append events logic here.
-        throw new NotImplementedException();
+        var eventData = events
+            .Select(ev => ev as DomainEvent)
+            .Select(ev => new EventData(Uuid.FromGuid(ev.Id), ev.GetType().Name, Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(ev))));
+
+        return await eventStore.AppendToStreamAsync(streamName, StreamState.Any, eventData, cancellationToken: ct);
     }
 
     [Fact]
@@ -65,18 +30,18 @@ public class GettingStateFromEventsTests
         var clientId = Guid.NewGuid();
         var shoesId = Guid.NewGuid();
         var tShirtId = Guid.NewGuid();
-        var twoPairsOfShoes = new PricedProductItem(new ProductItem(shoesId, 2), 100);
-        var pairOfShoes = new PricedProductItem(new ProductItem(shoesId, 1), 100);
-        var tShirt = new PricedProductItem(new ProductItem(tShirtId, 1), 50);
+        var twoPairsOfShoes = new ShoppingCartProductItem(shoesId, 100, 2);
+        var pairOfShoes = new ShoppingCartProductItem(shoesId, 100, 1);
+        var tShirt = new ShoppingCartProductItem(tShirtId, 50, 1);
 
-        var events = new object[]
+        var events = new DomainEvent[]
         {
             new ShoppingCartOpened(shoppingCartId, clientId),
-            new ProductItemAddedToShoppingCart(shoppingCartId, twoPairsOfShoes),
-            new ProductItemAddedToShoppingCart(shoppingCartId, tShirt),
-            new ProductItemRemovedFromShoppingCart(shoppingCartId, pairOfShoes),
-            new ShoppingCartConfirmed(shoppingCartId, DateTime.UtcNow),
-            new ShoppingCartCanceled(shoppingCartId, DateTime.UtcNow)
+            new ProductAddedToShoppingCart(shoppingCartId, twoPairsOfShoes),
+            new ProductAddedToShoppingCart(shoppingCartId, tShirt),
+            new ProductRemovedFromShoppingCart(shoppingCartId, pairOfShoes),
+            new ShoppingCartConfirmed(shoppingCartId),
+            new ShoppingCartCancelled(shoppingCartId)
         };
 
         await using var eventStore =
